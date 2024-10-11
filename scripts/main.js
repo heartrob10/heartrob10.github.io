f069282975de8a2d0a942c17e1348da86c47f8db
heartrob10 <robinson_galera@yahoo.com>
2024-10-11 20:33:11 +0800

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const body = document.body
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = Document.querySelector("#bye")
+
+spanHi.computedStyleMap.color = "red"
+spanHi.computedStyleMap.backgroundColor = "blue"
+
+
